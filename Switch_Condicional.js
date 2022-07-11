46cf007dbb5c8fd708d82d27db103e9041bf73f7
Jhorsax16 <jhorsax161616@gmail.com>
2022-07-11 00:50:58 -0500

    Switch y Operador ternario

---
+++ Switch_Condicional.js
@@ -0,0 +1,9 @@
+let num = 6
+
+switch(num) {
+    case 1: console.log('The number is 1')
+    break
+    case 2: console.log('The number is 2')
+    break
+    default: console.log('The number is not 1 or 2')
+}
---
+++ operador_Ternario.js
@@ -0,0 +1,15 @@
+/* Sintaxis del operador ternario
+
+1- (condición) ? true:false
+
+2- (condición) ?
+        (sentencia1,
+        sentencia2)
+        :
+        (sentencia1,
+        sentencia2)
+*/
+
+let num = 3;
+
+(num % 2 == 0) ? console.log(`${num} is even`) : console.log(`${num} is odd`)
